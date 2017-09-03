5a9d0aa0abf2998215166380698b513a67c18781
okuoku <mjt@cltn.org>
2017-09-03 10:31:34 +0900

    cimgui: Add cimgui

---
+++ externals/cimgui-srcs.cmake
@@ -0,0 +1,8 @@
+set(cimgui_srcs
+    cimgui/cimgui.cpp
+    cimgui/drawList.cpp
+    cimgui/fontAtlas.cpp
+    cimgui/listClipper.cpp
+    imgui/imgui.cpp
+    imgui/imgui_draw.cpp
+    imgui/imgui_demo.cpp)
